1250bd388965fb1f4ba1a1ff439005e14325928e
Smriti Eligar <smritieligar1005@gmail.com>
2024-12-21 05:29:12 +0530

    2.java

---
+++ Conditional_Statements/level 0/odd_eve.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sol{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    long a = sc.nextLong();
+    if(a%2==0){
+      System.out.print("EVEN");
+    }
+    else{
+      System.out.print("ODD");
+    }
+  }
+}
