921d0e3837a107dabb342c32671c111c52f3817d
Smriti Eligar <smritieligar1005@gmail.com>
2024-12-21 05:47:43 +0530

    4

---
+++ Conditional_Statements/level 1/upper_lowercase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.next().charAt(0);
+
+        if (letter >= 'a' && letter <= 'z') {
+            System.out.println("LOWERCASE");
+        } else if (letter >= 'A' && letter <= 'Z') {
+            System.out.println("UPPERCASE");
+        } else {
+            System.out.println("NONE");
+        }
+    }
+}
