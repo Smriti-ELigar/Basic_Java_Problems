bfe5f238a86661446af940286b353d6afbd2fd39
Smriti Eligar <smritieligar1005@gmail.com>
2024-12-21 05:58:56 +0530

    10

---
+++ Conditional_Statements/level 1/triangle.java
@@ -0,0 +1,16 @@
+// Write a program to check whether the given three sides can form a triangle or not.
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long s1 = scanner.nextLong();
+        long s2 = scanner.nextLong();
+        long s3 = scanner.nextLong();
+        if ((s1 + s2) > s3 && (s1 + s3) > s2 && (s2 + s3) > s1) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not Valid");
+        }
+    }
+}
